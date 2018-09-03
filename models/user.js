efc06120a61ac3abdbe7e0b1459e4c0251a04c59
aliyanamu <aliyanamu@gmail.com>
2018-09-03 21:45:27 +0700

    Edit production

--- models/user.js
+++ models/user.js
@@ -7,48 +7,50 @@ const hashPass = require('../helpers/hashPassword')
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {msg: 'Username is required'},
-        isUnique: function(value, next) {
-          User.findOne({
-            where: {
-              username: value,
-              id: {
-                [op.ne]: this.id
-              }
-            }
-          }).then(function(data){
-            if (data) next(`Username is already used`)
-            else next()
-          })
-        }
-      }
+      type: DataTypes.STRING
+      // type: DataTypes.STRING,
+      // validate: {
+      //   notEmpty: {msg: 'Username is required'},
+      //   isUnique: function(value, next) {
+      //     User.findOne({
+      //       where: {
+      //         username: value,
+      //         id: {
+      //           [op.ne]: this.id
+      //         }
+      //       }
+      //     }).then(function(data){
+      //       if (data) next(`Username is already used`)
+      //       else next()
+      //     })
+      //   }
+      // }
     },
     password: DataTypes.STRING,
     role: DataTypes.STRING,
     email: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {msg: 'Email is required'},
-        isEmail: {
-          args: true,
-          msg: 'Must be a valid email address'
-        },
-        isUnique: function(value, next) {
-          User.findOne({
-            where: {
-              email: value,
-              id: {
-                [op.ne]: this.id
-              }
-            }
-          }).then(function(data){
-            if (data) next(`Email is already used`)
-            else next()
-          })
-        }
-      }
+      type: DataTypes.STRING
+      // type: DataTypes.STRING,
+      // validate: {
+      //   notEmpty: {msg: 'Email is required'},
+      //   isEmail: {
+      //     args: true,
+      //     msg: 'Must be a valid email address'
+      //   },
+      //   isUnique: function(value, next) {
+      //     User.findOne({
+      //       where: {
+      //         email: value,
+      //         id: {
+      //           [op.ne]: this.id
+      //         }
+      //       }
+      //     }).then(function(data){
+      //       if (data) next(`Email is already used`)
+      //       else next()
+      //     })
+      //   }
+      // }
     }
   }, {});
   User.associate = function(models) {
