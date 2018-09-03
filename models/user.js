73b9773fd8e6767d100ade28b8e0e1906508c5ee
Vincent Wijaya <weqqss@yahoo.com>
2018-09-03 17:21:20 +0700

    Make username unique

--- models/user.js
+++ models/user.js
@@ -4,7 +4,10 @@ const { hashPassword } = require('../helpers/hash')
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
-    username: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      unique: true
+    },
     password: DataTypes.STRING,
     role: DataTypes.STRING
   }, {
