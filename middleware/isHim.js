5be42134283c56d713d0546c0cd2c8426a23b26b
Vincent Wijaya <weqqss@yahoo.com>
2018-09-03 17:57:48 +0700

    Final deploy

--- middleware/isHim.js
+++ middleware/isHim.js
@@ -1,6 +1,6 @@
 module.exports = {
   isHim: (req, res, next) => {
-    if(req.params.id === req.decoded.id) {
+    if(req.params.id == req.decoded.id) {
       next()
     }else {
       res.status(403).json({error: `You are not allowed to access user ${req.params.id}`})
