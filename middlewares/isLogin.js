e1999221e36867895108f1c41ba77899ee9bbd16
talkasrul <talkasrul@gmail.com>
2018-09-03 15:22:13 +0700

    isLogin and isAdmin middleware

---
+++ middlewares/isLogin.js
@@ -0,0 +1,25 @@
+const hash = require('../helpers/hashhelper')
+const Model = require('../models')
+
+
+module.exports = (req, res, next) => {
+    token = req.headers.token
+    if(token) {
+        let verify = hash.jwtdecode(token)
+        Model.User
+        .findOne({where:{id:verify.id}})
+        .then( response => {
+            if(response) {
+                next()
+            } else {
+                res.status(401).json({
+                    "message": "No Authorize"
+                })
+            }
+        })
+    } else {
+        res.status(401).json({
+            "message": "No Authenticate"
+        })
+    }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const { getAll, getOne, update, deleteOne, create } = require('../controllers/userController')
+const isLogin = require('../middlewares/isLogin')
+const isAdmin = require('../middlewares/isAdmin')
 
-router.get('/', getAll)
-router.get('/:id', getOne)
-router.post('/', create)
-router.put('/:id', update)
-router.delete('/:id', deleteOne)
+router.get('/', isAdmin, getAll)
+router.get('/:id', isLogin, getOne)
+router.post('/', isAdmin, create)
+router.put('/:id', isLogin, update)
+router.delete('/:id', isAdmin, deleteOne)
 
 module.exports = router
