1f7632e5f8be49f2cae5d304c098a9d5b6258260
ivan <ivansu.webmail@gmail.com>
2018-09-03 18:41:48 +0700

    Add / route

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use('/api', indexRouter);
+app.use('/', indexRouter);
 app.use('/api', usersRouter);
 
 // catch 404 and forward to error handler
