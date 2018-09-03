0c1500d5372ae3aaada77cb92b7403494860fa01
windra <pwindrap18@gmail.com>
2018-09-03 19:30:38 +0700

    done

--- controllers/userController.js
+++ controllers/userController.js
@@ -88,16 +88,11 @@ module.exports = {
     },
 
     getOneUser: function(req,res) {
-        const userId = req.decoded.id
         User.findById(req.params.id)
         .then(function(data){
-            if (data && userId==req.params.id) {
+            if (data) {
                 res.status(200).json({
-                    userData : data,
-                })
-            } else if (data && userId!=req.params.id) {
-                res.status(401).json({
-                    message : 'unauthorized'
+                    userData : data
                 })
             } else {
                 res.status(404).json({
@@ -136,7 +131,6 @@ module.exports = {
     },
 
     deleteUser : function(req,res) {
-        console.log('masukkkkkkkk')
         User.destroy({
             where : {
                 id : req.params.id
@@ -150,6 +144,34 @@ module.exports = {
         .catch(function(err){
             res.status(500).json({
                 message: err.message
+            })
+        })
+    },
+
+    updateUser : function (req,res) {
+        let salt = crypto.createHash('md5').update(req.body.email).digest('hex')
+        let combined = req.body.password + salt
+        let encryptedPassword = crypto.createHash('md5').update(combined).digest('hex')
+        User.update({
+            id : req.params.id,
+            firstName : req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: encryptedPassword
+        },{
+
+            where : {
+                id: req.params.id
+            }
+        })
+        .then(function(){
+            res.status(200).json({
+                message : 'success'
+            })
+        })
+        .catch(function(err){
+            res.status(500).json({
+                message : err.message
             })
         })
     }
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { signup, signin, getAllUsers, getOneUser, createUser, deleteUser} = require('../controllers/userController')
+const { signup, signin, getAllUsers, getOneUser, createUser, deleteUser, updateUser} = require('../controllers/userController')
 const isLogin = require('../middlewares/isLogin')
 const isAdmin = require('../middlewares/isAdmin')
+const isLoginUser = require('../middlewares/isLoginUser')
 
 /* GET users listing. */
 router.post('/signup',signup)
@@ -11,10 +12,12 @@ router.post('/signin',signin)
 
 router.get('/users',isLogin,isAdmin,getAllUsers)
 
-router.get('/users/:id',isLogin,getOneUser)
+router.get('/users/:id',isLogin,isLoginUser,getOneUser)
 
 router.post('/users',isLogin,isAdmin,createUser)
 
 router.delete('/users/:id',isLogin,isAdmin,deleteUser)
+
+router.put('/users/:id',isLogin,isLoginUser,updateUser)
 
 module.exports = router;
