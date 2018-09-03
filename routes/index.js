5a174d45773238751f4d6a6abfad164822e4ecbb
imamaguss <imam.agussopian@gmail.com>
2018-09-03 21:39:11 +0700

    update inde

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send('ada');
+  res.status(200).json({message: 'Welcome to e-See Laundry...'});
 });
 
 module.exports = router;
