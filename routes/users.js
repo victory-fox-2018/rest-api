caf071602d9ceee7139fb07af14e8e9469ee67fe
imamaguss <imam.agussopian@gmail.com>
2018-09-03 21:46:01 +0700

    done

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ const {isAdmin} = require('../midleware/isAdmin');
 
 router.post('/signup', User.signup);
 router.post('/signin', User.signin);
-router.get('/', User.findAll);
-router.get('/:id', User.findOne);
+router.get('/', isLogin, isAdmin, User.findAll);
+router.get('/:id', isLogin, User.findOne);
 router.post('/', isLogin, isAdmin, User.create);
 router.delete('/:id', isLogin, isAdmin, User.erase);
 router.put('/:id', isLogin, User.update);
