28861f01a3b6ccadd733460b19d87dc264b9fd8b
ridhoemgl <ridhoemgl@gmail.com>
2018-09-03 19:41:34 +0700

    initial add

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const userRoutes = require('./routes/userRoutes')
 require('dotenv').config()
 
 const app = express()
+const port = process.env.PORT || 4000
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/api', userRoutes)
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000')
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
 })
