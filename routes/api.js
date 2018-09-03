8086b3ba847adf3c11ac0cf79b0eea59e5decc9e
harlesbayu <bayuanggaraharles@gmail.com>
2018-09-03 20:23:21 +0700

    heroku

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,8 @@ routes.post('/signup', apiController.signup)
 routes.post('/signin', apiController.signin)
 routes.get('/users', isLogin, isAdmin ,apiController.findAll)
 routes.get('/users/:id', isLogin , apiController.getOne)
-routes.post('/users', isAdmin , apiController.create)
-routes.delete('/users/:id', isAdmin, apiController.remove)
+routes.post('/users', isLogin, isAdmin , apiController.create)
+routes.delete('/users/:id', isLogin, isAdmin, apiController.remove)
 routes.put('/users/:id', isLogin, apiController.update)
 
 
